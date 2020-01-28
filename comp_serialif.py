7b4300297a9f177079c5d684c530671506edd863
Cody <carobers@asu.edu>
2020-01-28 11:33:10 -0700

    Added Serial Interface and Mod gui
    The serial interface has been created and stat updating has been implemented
    in the gui

---
+++ comp_serialif.py
@@ -0,0 +1,149 @@
+"""
+author: Cody Roberson
+date: 2020-01-28
+desc: This is the serial interface for the compressor
+"""
+
+import serial
+import time
+import pycrc
+
+
+class f70L_HeCompressor():
+    """
+    Class:  f70L_HeCompressor\n
+    Description:    Sumitomo (SHI) Cryogenics of America Helium Compressor Model F-70L Control Interface\n
+    Input: String:= Serial Port\n
+    """
+    def __init__(self, port):
+        self.serialConnection = serial.Serial()
+        self.serialConnection.baudrate = 9600
+        self.serialConnection.bytesize = 8
+        self.serialConnection.parity = PARITY_NONE
+        self.serialConnection.timeout = 2
+        self.serialConnection.port = port
+        
+
+    def startConnection(self):
+        """
+        Function: startConnection()\n
+        Description: The configuration for the serial interface is already set other than the port. This is called to actually open the port.
+        Input: none
+        Output: Boolean:= True (Port opened) / False (Failed to open port)
+        """
+        try:
+            self.serialConnection.open()
+            if self.serialConnection.is_open:
+                return True
+        except SerialException:
+            print("There was an issue establishing the serial connection")
+            return False
+
+
+    def collectStats(self):
+        """
+        Class Function: collectStas()\n
+        Description: Communicates with the Helium Compressor over serial to read various stats\n
+        Input: None\n
+        Output: List:= [Capsule Helium Discharge Temp, water outlet Temp, Water inlet Temp, Compressor return temp]\n
+                None:= in the case that there is no connection, None is returned
+        todo: Future Release: Implement 'read status bits' \n
+        """
+        if not self.serialConnection.is_open:
+            return None
+        compressorStats = []
+        # First send the command to get me my temps
+        readTempStr = b"$TEAA4B9\r" # This has the checksum already included
+        self.serialConnection.write(readTempStr)
+        tempReply = self.serialConnection.readLine()
+
+        
+
+        readPressureStr = b"$PR171F6\r" # this also has checksum included
+        self.serialConnection.write(readPressureStr)
+        preaReply = self.serialConnection.readLine()
+        
+        # Check reply length and checksum for temperature
+        """
+        Example Reply
+        $TEA,086,040,031,000,3798<cr>
+        """
+        if len(tempReply) > 0:
+            rep = tempReply[0:len(tempReply)-5].encode("ASCII") # extract reply without crc
+            calccrc = hex(pycrc.crc16_modbus(rep))[2:]   #calculate crc of packet and convert to str
+            retcrc = tempReply[len(tempReply)-5:-1] # extract crc 
+            if calccrc == retcrc:
+                subst = tempReply.split()
+                compressorStats.append(subst[1])
+                compressorStats.append(subst[2])
+                compressorStats.append(subst[3])
+
+
+        # Check reply length and checksum for pressure
+        """
+        Example Reply
+        $PR1,079,2EBD<cr>
+        """
+        
+        if len(preaReply) > 0:
+            rep = preaReply[0:len(preaReply)-5].encode("ASCII") # extract reply without crc
+            calccrc = hex(pycrc.crc16_modbus(rep))[2:]   #calculate crc of packet and convert to str
+            retcrc = preaReply[len(preaReply)-5:-1] # extract crc 
+            if calccrc == retcrc:
+                subst = preaReply.split()
+                compressorStats.append(subst[1])
+
+        # Finally, Return Stats
+        return compressorStats
+
+
+
+    def turnOn(self):
+        """
+        Class Function:   \n
+        Description:   \n
+        Input:   \n
+        Output:  \n
+        Notes:  When the compressor is off and without active fault, this will turn the compressor and  \n
+                cold head on. If the command is sent while the compressor is in other states, the RS232  \n
+                response will be returned, but no action will occur.  \n
+        """
+        if not self.serialConnection.is_open:
+            return None
+        command = b"$ON177CF\r" # Precalculated command + checksum
+        expectedReply = "$ON1,8936\r" #Precalculated reply+checksum
+        self.serialConnection.write(command)
+        reply = self.serialConnection.readLine()
+
+        if reply == expectedReply:
+            print("Command: Turn On, was successfully sent")
+        else:
+            print("An error has occured while attempting the command: Turn on")
+            print("Command:= {}\nReply:= {}".format(command, reply))
+
+
+    def turnOff(self):
+        """
+        Class Function:   \n
+        Description:   \n
+        Input:   \n
+        Output:  \n
+        Notes:  When the compressor and/or cold head is on, this will turn either or both off. If the \n
+                command is sent while the compressor and cold head are off, the RS232 response will \n
+                be returned, but no action will occur.  \n
+        """
+        if not self.serialConnection.is_open:
+            return None
+        command = b"$OFF9188\r" # Precalculated command + checksum
+        expectedReply = "$OFF,bb90\r" #Precalculated reply + checksum
+        self.serialConnection.write(command)
+        reply = self.serialConnection.readLine()
+
+        if reply == expectedReply:
+            print("Command: Turn On, was successfully sent")
+        else:
+            print("An error has occured while attempting the command: Turn on")
+            print("Command:= {}\nReply:= {}".format(command, reply))
+
+
+
--- gui.py
+++ gui.py
@@ -1,31 +1,76 @@
 """
-
-
-
+author: Cody Roberson
+Date: 2020-01-28
+purpose: Serves as the main gui for the cryo controller
+@copyright 2020 Arizona State University
 """
 import tkinter as tk
+import comp_serialif
+
+## Serial Port ##
+srCnxn = "/dev/ttyUSB0"
 
 class App(tk.Tk):
     def __init__(self):
         super().__init__()
+
+        """
+            Initalize serial connection to compressor
+        """
+
+        self.compressor = comp_serialif.f70L_HeCompressor(srCnxn)
+
+        """
+        Group 1 gui Elements.
+        """
+        
         group_1 = tk.LabelFrame(self, padx=25, pady=15,
                                 text="Tempreture and Pressure")
         group_1.pack(padx=10, pady=5)
 
         tk.Label(group_1, text="Compressor Capsule Helium Discharge Temperature: ").grid(row=0)
+        self.compressorcapsuletemp = tk.Label(group_1, text="Fetching Data").grid(row=0, column=1)
+       
         tk.Label(group_1, text="Water Outlet Temperature: ").grid(row=1)
+        self.outletTemp = tk.Label(group_1, text="Fetching Data").grid(row=1, column=1)
+       
         tk.Label(group_1, text="Water inlet Temperature: ").grid(row=2)
+        self.inletTemp = tk.Label(group_1, text="Fetching Data").grid(row=2, column = 1)
+       
         tk.Label(group_1, text="Compressor Return Pressure: ").grid(row = 3)
+        self.returnPressure = tk.Label(group_1, text="Fetching Data").grid(row=3, column=1)
 
+        """
+        Group 2 gui elements
+        """
         group_2 = tk.LabelFrame(self, padx=25, pady=15, text="Controls")
         group_2.pack(padx=10, pady=5)
 
         tk.Button(group_2, text="Compressor On").grid(row=0)
         tk.Button(group_2, text="Compressor Off").grid(row=0, column=1)
+        
+        """     
+        TODO: Not Implemented in interface
         tk.Button(group_2, text="Cold Head Run").grid(row=1, column=0)
         tk.Button(group_2, text="Cold Head Pause").grid(row=1, column = 1)
-        tk.Button(group_2, text="Cold Head Pause Off").grid(row=1, column = 2)
+        tk.Button(group_2, text="Cold Head Pause Off").grid(row=1, column = 2) 
+        """
+   
+        self.updateStats()
 
+
+    """
+    Setup a timer to get the status of the compressor
+    """
+    def updateStats(self):
+        print("Fetching Data")
+        data = self.compressor.collectStats()
+        if data not None:
+            self.compressorcapsuletemp = data[0] + " °C"
+            self.outletTemp = data[1] + " °C"
+            self.inletTemp = data[2] + " °C"
+            self.returnPressure = data[3] + " PSIG"
+            self.after(5000, self.updateStats)
 
 
 
